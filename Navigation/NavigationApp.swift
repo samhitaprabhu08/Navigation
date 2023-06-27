1031d01f6566bab9c1adc61e62a55d00e42c86a1
Samhita <meetu2005@icloud.com>
2023-06-27 10:08:30 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Samhita Prabhu on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
